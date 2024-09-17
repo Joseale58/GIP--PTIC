7dafb14f42c188d3b26465527e309e8b81d2e4ce
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-09-17 18:16:23 -0500

    feat: :sparkles: receive 2 files from front-end

--- application/DataUploadController.py
+++ application/DataUploadController.py
@@ -1,25 +1,28 @@
 from fastapi import UploadFile, APIRouter
-from pydantic import BaseModel
-from infrastructure.models.UserModel import User
+
 from domain.DataUploadService import DataUploadService
+from infrastructure.models.UserModel import User
 
 DataUploadController = APIRouter(prefix="/data-upload")
 
+
 @DataUploadController.post("/")
-def data_upload(file: UploadFile):
-    print(file.filename)
+def data_upload(rips: UploadFile, monthly: UploadFile):
+    print(rips.filename)
+    print(monthly.filename)
     # Acá irá la lógica para convertir el archivo a un arreglo de objetos de tipo BaseFileEntity
     # Ese arreglo se lo debo enviar a la función clean data en vez del arreglo vacío
 
     return DataUploadService.clean_data([])
 
 
-#Controlador de e.g. insertar usuario
+# Controlador de e.g. insertar usuario
 @DataUploadController.post("/create-user")
 def insert_user(user: User):
     return DataUploadService.insert_user(user)
 
-#Controlador de e.g. traer todos los usuarios
+
+# Controlador de e.g. traer todos los usuarios
 @DataUploadController.get("/")
 def get_data():
     return DataUploadService.get_data()
