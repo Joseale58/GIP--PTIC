53c8c5a59e6d175ace63b78da03e8e57009c1718
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-09-22 10:54:14 -0500

    feat: transform excel files to pandas dataframes

--- application/DataUploadController.py
+++ application/DataUploadController.py
@@ -1,4 +1,7 @@
+from io import BytesIO
+
 from fastapi import UploadFile, APIRouter
+from pandas import read_excel
 
 from domain.DataUploadService import DataUploadService
 from infrastructure.models.UserModel import User
@@ -7,22 +10,24 @@ DataUploadController = APIRouter(prefix="/data-upload")
 
 
 @DataUploadController.post("/")
-def data_upload(rips: UploadFile, monthly: UploadFile):
-    print(rips.filename)
-    print(monthly.filename)
-    # Acá irá la lógica para convertir el archivo a un arreglo de objetos de tipo BaseFileEntity
-    # Ese arreglo se lo debo enviar a la función clean data en vez del arreglo vacío
+async def data_upload(rips: UploadFile, monthly: UploadFile):
+    rips_file = await rips.read()
+    rips_df = read_excel(BytesIO(rips_file))
+    monthly_file = await monthly.read()
+    monthly_df = read_excel(BytesIO(monthly_file))
 
-    return DataUploadService.clean_data([])
+    return DataUploadService.clean_data(rips_df, monthly_df)
 
 
 # Controlador de e.g. insertar usuario
+# Esto debería ir en otro controller
 @DataUploadController.post("/create-user")
 def insert_user(user: User):
     return DataUploadService.insert_user(user)
 
 
 # Controlador de e.g. traer todos los usuarios
+# Esto también
 @DataUploadController.get("/")
 def get_data():
     return DataUploadService.get_data()
--- domain/DataUploadService.py
+++ domain/DataUploadService.py
@@ -1,27 +1,25 @@
-from typing import List
+from pandas import DataFrame
 
-from domain.models.BaseFileEntityModel import BaseFileEntity
 from infrastructure.DataUploadRepository import DataUploadRepository
 
 
 class DataUploadService:
     @staticmethod
-    def clean_data(data_from_file: List[BaseFileEntity]):
-        # Por defecto recibe un arreglo con las filas del archivo que se envía
-        print("Acá irá todo el código de la limpieza y eliminación de nulos")
-        # Debe retornar un arreglo con los nuevos elementos después de la limpieza, imputación, etc.
-        # Esto para poder llamar la función: DataUploadRepository.insert() y pasarle ese arreglo en vez del vacío
+    def clean_data(rips_df: DataFrame, monthly_df: DataFrame):
+        print(rips_df.info())
+        print(monthly_df.info())
+        # Acá te mando los dos dataframes que se envían de los archivos de excel
+        # Leete el readme de como probar esto desde un postman
 
         return DataUploadRepository.insert([])
-    
 
-    #Método de e.g. insertar usuario
+    # Método de e.g. insertar usuario
     @staticmethod
     def insert_user(user):
-        #Se deben todas las validaciones correspondientes
+        # Se deben todas las validaciones correspondientes
         return DataUploadRepository.insert_user(user)
-    
-    #Método de e.g. para traer todos los usuarios
+
+    # Método de e.g. para traer todos los usuarios
     @staticmethod
     def get_data():
         return DataUploadRepository.get()
